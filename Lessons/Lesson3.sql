a37c4cb80f9cc37758098a7490b685afa74bd3b5
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-03-31 00:00:31 +0300

    Update SQL

--- Lessons/Lesson3.sql
+++ Lessons/Lesson3.sql
@@ -16,6 +16,6 @@ CREATE TABLE mytable (
     "id", "first_name", "last_name", "email", "phone", "address", "city", and "country".
     The "id" column is defined as an integer and set as the primary key.
     the "first_name" and "last_name" columns are defined as strings with a maximum length of 50 characters,
-    and the "email", "phone", "address", "city", and "country"  columns are 
+    and the "email", "phone", "address", "city", and "country"  columns are
     defined as strings with a maximum length of 50, 20, 100, 50, and 50 characters respectively. 
     The "NOT NULL" constraint is used to ensure that values are always provided for the "id", "first_name", and "last_name" columns  . 
