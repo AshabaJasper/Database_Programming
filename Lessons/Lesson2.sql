f28a24b732b1a0681393da2e92d2918670f7e5c2
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-05-02 20:45:07 +0300

    Update

--- Lessons/Lesson2.sql
+++ Lessons/Lesson2.sql
@@ -13,13 +13,13 @@ CREATE TABLE mytable (
   name VARCHAR(50) NOT NULL,
   age INT 
 );    
-   
+    
 -- This inserts a new row into mytable with values for id, name, and age
 INSERT INTO mytable (id, name, age) VALUES (1, 'John', 30);
  
 -- This updates the age column for the row with id 1 in mytable
 UPDATE mytable SET age = 35 WHERE id = 1;
-
+ 
 -- This deletes the row with id 1 from mytable
 DELETE FROM mytable WHERE id = 1;
 
