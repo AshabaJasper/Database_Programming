54c1eaaf0b8ec6119bff6773197e2ed269c77362
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-10-04 22:41:07 +0300

    Update Lesson1.sql

--- Lessons/Lesson1.sql
+++ Lessons/Lesson1.sql
@@ -11,7 +11,7 @@ USE myfirstDB;
    
 -- This drops (deletes) the myfirstDB database
 DROP DATABASE myfirstDB;
- -- 
+
 -- This sets the myfirstDB database to read-only mode, preventing any changes
 ALTER DATABASE myfirstDB READ ONLY = 1;
    
