1b1e511110fa2efb1d36e2a766cf914aa7db114e
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-10-13 12:38:01 +0300

    Update Lesson1.sql

--- Lessons/Lesson1.sql
+++ Lessons/Lesson1.sql
@@ -2,7 +2,7 @@
 
 -- double dashes (--) are used to indicate comments in SQL code. The comments are not actually executed by the SQL engine,
 -- but can be used to explain what the code does or provide other useful information.
---
+
 -- This creates a new database called myfirstDB
 CREATE DATABASE myfirstDB;
 
