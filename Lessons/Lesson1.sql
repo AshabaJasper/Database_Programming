ad2160f65d12c2c581e93c402f9cf67a64bfaafd
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-12-04 21:39:24 +0300

    Update Lesson1.sql

--- Lessons/Lesson1.sql
+++ Lessons/Lesson1.sql
@@ -1,5 +1,5 @@
 -- INTRODUCTION
---Resrtucture SQL Lessons on Sunday--
+--Resrtucture SQL Lessons on Sunday
 -- double dashes (--) are used to indicate comments in SQL code. The comments are not actually executed by the SQL engine,
 -- but can be used to explain what the code does or provide other useful information.
 
